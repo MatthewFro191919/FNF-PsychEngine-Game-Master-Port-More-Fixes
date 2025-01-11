22b06f9d9091e1f0d69bfa72453d80bf09145ee5
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-01-11 08:25:03 -0500

    Add files via upload

---
+++ assets/base_game/shared/data/jovial/color.lua
@@ -0,0 +1,5 @@
+function onCreate()
+setProperty("boyfriendGroup.color", getColorFromHex("0xFF823C25"))
+setProperty("gfGroup.color", getColorFromHex("0xFF823C25"))
+setProperty("dadGroup.color", getColorFromHex("0xFF823C25"))
+end
